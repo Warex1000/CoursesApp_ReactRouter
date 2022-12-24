eeb8f130247a860faea3ef3f2983ff9fd73b26b7
Artem <89835583+Warex1000@users.noreply.github.com>
2022-12-24 22:49:45 +0200

    Add files via upload

---
+++ src/components/SingleCourse.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import courses from '../data/courses'
+
+const SingleCourse = () => {
+  const params = useParams()
+  const navigate = useNavigate()
+  const course = courses.find((course) => course.slug === params.courseSlug)
+  useEffect(() => {
+    if (!course) {
+      navigate('..', { relative: 'path' })
+    }
+  }, [course, navigate])
+
+  return (
+    <>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
+      <Link to=".." relative="path">
+        All courses
+      </Link>
+    </>
+  )
+}
+
+export default SingleCourse
